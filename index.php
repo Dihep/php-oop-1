22a4473f6253d716f7df2843f305ab7f876bd596
Dihep <dihep.demayo@gmail.com>
2024-06-27 21:13:30 +0200

    Istanza e stampa degli oggetti.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Movie {
     public $genre;
     public $poster;
 
-    public function __construct($title, $description, $releaseDate, $lenght, $genre, $poster)
+    public function __construct($title, $description, $releaseDate, $lenght, $genre)
     {
         $this->title = $title;
         $this->description = $description;
@@ -23,9 +23,24 @@ class Movie {
         echo "<li>Descrizione: " . $this->description . "</li>";
         echo "<li>Data di uscita: " . $this->releaseDate . "</li>";
         echo "<li>Durata: " . $this->lenght . "</li>";
-        echo "<li>Genere: " . $this->genre . "</li>";
+        echo "<li>Genere: " . $this->genre . "</li></ul>";
     }
 }
+
+$movie1 = new Movie(
+    "Iron man", 
+    "Descrizione del film di Iron Man",
+    "18-18-1888",
+    "2 ore",
+    "azione"
+);
+$movie2 = new Movie(
+    "Gold man", 
+    "Descrizione del film di Gold Man",
+    "13-13-1333",
+    "3 ore",
+    "romantico"
+    )
 
 ?>
 
@@ -35,12 +50,14 @@ class Movie {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie OOP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     
     <?php
-        
+        $movie1->print();
+        $movie2->print();
     ?>
 
 </body>
